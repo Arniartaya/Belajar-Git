3e7d629325c6726a6a14ea0c2e9a233da5b81551
Arniartaya <arniarta9@gmail.com>
2022-07-08 15:07:23 +0700

    belajar switch

---
+++ Jumat_8_Juli.js
@@ -0,0 +1,12 @@
+var a = 4
+
+function checkData() {
+    if (a == 3) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+console.log(checkData());
---
+++ main.js
@@ -0,0 +1,40 @@
+// // const myHeading = document.querySelector('h1');
+// // myHeading.textContent = 'Hello world!';
+
+// // let botol = "air"
+// // console.log(123);
+
+// // var a = 5
+// // var b = 6
+// // var c = a + b;
+// // console.log(c);
+
+
+// // console.log(a + b);
+// // console.log(a * b);
+// // console.log(a / b);
+// // console.log(a - b);
+
+// if (a !== b) {
+//     console.log(false);
+// } else {
+//     console.log(true);
+// }
+var iceCream = 'chocolate';
+
+function cokobeli() {
+
+    if (iceCream !== 'chocolate') {
+        return ('Yay, I love chocolate ice cream!');
+    } else {
+        return ('Awwww, but chocolate is my favorite...');
+    }
+
+}
+
+console.log(cokobeli());
+
+
+
+// var a = 10
+// var b = 12
