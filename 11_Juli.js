9d75e31b2e94467d2964d94484755cf52782a465
Arniartaya <arniarta9@gmail.com>
2022-07-11 16:45:01 +0700

    looping

---
+++ 11_Juli.js
@@ -0,0 +1,23 @@
+// // Var Ulang = true;
+// // while (Ulang === true) {
+// //     console.log('hello world');
+// //     Ulang = confirm('lagi');
+// // }
+
+// var nilaiAwal = 1;
+// while (nilaiAwal <= 5) {
+//     console.log('Hello World!');
+//     nilaiAwal + 1;
+// }
+
+for (let line = "#"; line.length < 8; line += "#")
+    console.log(line);
+
+for (let index = 0; <
+    array.length; index++) {
+    const element = array[index];
+    (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+    }
+}
